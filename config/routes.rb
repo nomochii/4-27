8594071570baabadf08f1e83a68c32d3c2e71214
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-06-25 15:33:26 +0000

    [Add]リンク先の設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "/homes/about" => "homes#about", as: "about"
+  patch 'books/:id' => 'books#update', as: 'update_book'
 
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books
   resources :users, only: [:show, :index, :edit, :update]
 
 end
