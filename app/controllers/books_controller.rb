eceffe7e47b34ecf3293dc2d208f02838e2649ad
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-07-02 12:46:47 +0000

    新規投稿時のエラー解決

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,10 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    #@book = Book.new
     if @book.save
-      redirect_to  @book
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to  book_path(@book.id)
     else
       render :index
     end
@@ -12,12 +14,14 @@ class BooksController < ApplicationController
 
 
   def index
+    @user = current_user
     @books = Book.all
     @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
@@ -26,8 +30,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(book.id)
+    if @book.update(book_params)
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
