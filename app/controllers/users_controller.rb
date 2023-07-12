56722427a005ebde41a7f56b4139e7f582129c8e
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-07-12 15:54:55 +0000

    ログインのメールを名前に変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,19 @@ class UsersController < ApplicationController
     @newbook = Book.new
   end
 
-  # def create
-  #   @user = User.new(user_params)
-  #   if @user.save
-  #     flash[:success] = 'Welcome! You have signed up successfully.'
-  #     redirect_to login_path
-  #   else
-  #     render :new
-  #   end
-  # end
+  def new
+    @user = User.new
+  end
+
+   def create
+     @user = User.new(user_params)
+     if @user.save
+       flash[:success] = 'Welcome! You have signed up successfully.'
+       redirect_to login_path
+     else
+       render :new
+     end
+   end
 
   def index
     @user = current_user
